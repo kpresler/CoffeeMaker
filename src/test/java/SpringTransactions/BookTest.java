dcd6c8f715b45ddb1089ea70350072a4a4d655f9
Kai Presler-Marshall <kpresle@ncsu.edu>
2020-12-02 17:12:26 -0500

    Switch to MySQL working, transaction rollback appears to work too

--- src/test/java/SpringTransactions/BookTest.java
+++ src/test/java/SpringTransactions/BookTest.java
@@ -6,25 +6,25 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import SpringTransactions.models.User;
-import SpringTransactions.repositories.BookRepository;
-import SpringTransactions.repositories.UserRepository;
-import SpringTransactions.services.BookService;
 import SpringTransactions.services.UserService;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {BookService.class, UserService.class, BookRepository.class, UserRepository.class})
+@EnableAutoConfiguration
+//@ContextConfiguration(classes = {BookService.class, UserService.class})
+@SpringBootTest (classes=TestConfig.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
 public class BookTest {
 	
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private BookService bookService;
-	
 	
 	@Test
 	public void createBooks() {
---
+++ src/test/java/SpringTransactions/TestConfig.java
@@ -0,0 +1,9 @@
+package SpringTransactions;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({ "SpringTransactions" })
+public class TestConfig {
+}
