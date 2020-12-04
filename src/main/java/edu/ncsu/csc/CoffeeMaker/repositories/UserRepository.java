7d6e9e7ddf159c3a033e3922ba899cd97a93f0fd
Kai Presler-Marshall <kpresle@ncsu.edu>
2020-12-04 18:52:15 -0500

    Remove classes from starting example

--- src/main/java/edu/ncsu/csc/CoffeeMaker/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.ncsu.csc.CoffeeMaker.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.ncsu.csc.CoffeeMaker.models.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
